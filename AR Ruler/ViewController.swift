6427eede157dcb9e0bd7e1efc714ec978d82f518
Jakub1984 <j.perich@seznam.cz>
2019-01-08 16:03:09 +0630

    Added AR front and end nodes

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,21 +13,12 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var markerNodeArray = [SCNNode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,29 +38,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchLocation = touches.first?.location(in: sceneView) else {return}
+        guard let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint).first else {return}
+        addMarker(at: hitTestResult)
+        print(markerNodeArray.count)
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func addMarker(at hitTestResutlt: ARHitTestResult) {
+        let marker = SCNCylinder(radius: 0.003, height: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        marker.materials = [material]
+        let markerNode = SCNNode(geometry: marker)
+        let location = hitTestResutlt.worldTransform.columns.3
+        markerNode.position = SCNVector3(location.x, location.y, location.z)
+        
+        markerNodeArray.append(markerNode)
+
+        if markerNodeArray.count != 3 {
+            sceneView.scene.rootNode.addChildNode(markerNode)
+        }
+        
+        if markerNodeArray.count > 2 {
+            markerNodeArray[0].removeFromParentNode()
+            markerNodeArray[1].removeFromParentNode()
+            markerNodeArray.removeAll()
+        }
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
